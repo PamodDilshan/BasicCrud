aa99aea6a6e43042fd13035bfe8e537ada7815c6
Pamod Dilshan <pamod@brylix.com>
2024-11-19 19:27:54 +0530

    second

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import './Home.css'
+import { type } from '@testing-library/user-event/dist/type';
 
 const Home = () => {
   const list = [
@@ -10,25 +11,46 @@ const Home = () => {
   ];
   
   const [lists, setList] = useState(list);
+  const [updateState, setUpdateState] = useState(-1)
 
   return (
     <div className="home">
       <h1>Gneuro Engineering PVT Limited</h1>
+    
       <AddList setList={setList}/>
+      <form>
       {
         lists.map((current) => (
+          updateState === current.id ? <EditList current={current}/> :
           <div key={current.id}>
             <div>{current.name}</div>
             <div>
-              <button> Edit </button> 
+              <button onClick={() => handleSubmit(current.id)}> Edit </button> 
               <button> Delete </button>
             </div>
           </div>
         ))
       }
+    </form>
     </div>
   );
+
+  function handleSubmit(id) {
+    setUpdateState(id)
+  }
+
 }
+
+function EditList({current}){
+  return(
+    <>
+    <input type="text" name='name' value={current.name}/>
+    <button type='submit'> Update </button>
+    </>
+  )
+}
+
+
 function AddList({setList}) {
     const nameRef = useRef()
 
